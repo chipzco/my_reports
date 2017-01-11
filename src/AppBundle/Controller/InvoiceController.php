0399d3070a80f26ec3463d226a507b0d54a10545
charles <charles.daniel@computerwebservices.net>
2017-01-10 22:29:38 -0500

    continue inv. rep.

--- src/AppBundle/Controller/InvoiceController.php
+++ src/AppBundle/Controller/InvoiceController.php
@@ -1,20 +1,31 @@
 <?php
 namespace AppBundle\Controller;
-use AppBundle\Entity\Video;
-use AppBundle\Form\VideoFrm;
+use AppBundle\Entity\{InvoiceCharge,InvoiceDetail,InvoiceHeader};
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\BrowserKit\Response;
 
 
 
 class InvoiceController extends Controller {
 	/**
-	 * @Route("/video/add", name="videoadd")
+	 * @Route("/invoice/rep1", name="invoice_rep1")
 	 */
 	public function repFormAction(Request $request)
 	{
-		$video=new Video();
+		return $this->render('invoice/rep1form.html.twig');
 		
 	}
+	
+	/**
+	 * @Route("/invoice/repgen",name="invoice_reportgen")
+	 */
+	public function repGenAction(Request $request) {
+		$frm=$request->request;
+		 
+		print_r($frm);
+		die;		
+	}
+	
 }
